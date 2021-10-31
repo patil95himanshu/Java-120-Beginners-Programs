1a302ac2afe581e9d9fe1988acdf9923e0f7dee0
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-11-01 00:02:34 +0530

    Triangular Pattern-4.

---
+++ Program24/src/Program24/FourthPattern.java
@@ -0,0 +1,19 @@
+package Program24;
+/**
+* @author -- Himanshu Patil
+*/
+public class FourthPattern {
+	public static void main(String args[])
+	{
+		int x,y;
+		for(x=1; x<=7; x++)
+		{
+			for(y=1; y<x; y++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
