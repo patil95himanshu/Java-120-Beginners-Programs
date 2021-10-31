850acbc65b6d9b3a502170293750d1923c1a9891
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-10-31 16:27:16 +0530

    Basic Arithmetic Calculators.

---
+++ Program3/src/Arithmetic/Maths.java
@@ -0,0 +1,24 @@
+package Arithmetic;
+/**
+* @author -- Himanshu Patil
+*/
+public class Maths {
+	
+	public static void main(String args[]) {
+		
+		int num1= 5, num2 = 10, Result1,Result2,Result3;
+		
+		Result1 = num1 + num2;
+		Result2 = num2 - num1;
+		Result3 = num1 * num2;
+		float Result4 = num2 % num1;
+		
+		System.out.println("Result of Addition is: " + Result1);
+		System.out.println("Result of Substraction is: " + Result2);
+		System.out.println("Result of Multiplication is: " + Result3);
+		System.out.println("Result of Division is: " + Result4);
+		
+		
+	}
+
+}
