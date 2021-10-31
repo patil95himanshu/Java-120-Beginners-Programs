245314459e6d4ecd160af436e47e2bb74f7b2515
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-11-01 00:01:18 +0530

    Triangular Pattern-3.

---
+++ Program23/src/Triangle/ThirdPattern.java
@@ -0,0 +1,20 @@
+package Triangle;
+/**
+* @author -- Himanshu Patil
+*/
+public class ThirdPattern {
+	
+	public static void main(String args [])
+	{
+		int x,y;
+		for(x=7; x>=1; x--)
+		{
+			for(y=1;y<=x;y++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
