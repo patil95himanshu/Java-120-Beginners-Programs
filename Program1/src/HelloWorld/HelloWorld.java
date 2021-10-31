f1d1da5bdeb59885d786eed288d0ca29f892647e
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-10-31 16:25:30 +0530

    Hello World.

---
+++ Program1/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+/**
+* @author -- Himanshu Patil
+*/
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World");
+	}
+
+}
