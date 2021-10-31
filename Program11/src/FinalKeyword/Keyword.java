226e7d1c9220475e402bd1fbc65511478429adb4
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-10-31 20:27:35 +0530

    Use of Final Keyword as a method.

---
+++ Program11/src/FinalKeyword/Keyword.java
@@ -0,0 +1,21 @@
+package FinalKeyword;
+/**
+* @author -- Himanshu Patil
+*/
+public class Keyword {
+	
+	final void display()
+	{
+		System.out.println("This is a final Method");
+	}
+
+}
+
+class Hello extends Keyword {
+	
+	public static void main (String args [])
+	{
+		Hello h1 = new Hello();
+		h1.display();
+	}
+}
