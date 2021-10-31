01bd66348fb73a6c10ff08faaed3c6646d20d038
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-11-01 02:19:30 +0530

    Print out Arrays.

---
+++ Program37/src/PrintArray/Array.java
@@ -0,0 +1,17 @@
+package PrintArray;
+/**
+* @author -- Himanshu Patil
+*/
+public class Array {
+	
+	public static void main(String args [])
+	{
+		int x[] = {1,2,3,4,5,6,7,8};
+		System.out.println("The Array is: ");
+		for(int y = 0; y < x.length; y++)
+		{
+			System.out.print(" " + x[y]);
+		}
+	}
+
+}
