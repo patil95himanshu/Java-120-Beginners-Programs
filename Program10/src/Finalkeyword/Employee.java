aa75a8c7d65c5e70804a936578ab9bfa4cf4fa05
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-10-31 16:31:42 +0530

    Use of Final Keyword as a variable.

---
+++ Program10/src/Finalkeyword/Employee.java
@@ -0,0 +1,26 @@
+package Finalkeyword;
+/**
+* @author -- Himanshu Patil
+*/
+
+public class Employee {
+	
+	final int Reg_No;
+	Employee(int num)
+	{
+		Reg_No = num;
+	}
+	void display()
+	{
+		System.out.println("Employee number is: " + Reg_No);
+	}
+	
+	public static void main(String args [])
+	{
+		Employee em = new Employee(101);
+		em.display();
+	}
+	
+
+}
+
