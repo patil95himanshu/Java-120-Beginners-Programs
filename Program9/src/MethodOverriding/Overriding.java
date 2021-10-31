75c59e28f08cd81df696bcf549b39fd838375857
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-10-31 16:31:03 +0530

    Method Overriding.

---
+++ Program9/src/MethodOverriding/Overriding.java
@@ -0,0 +1,46 @@
+package MethodOverriding;
+/**
+* @author -- Himanshu Patil
+*/
+
+class First
+{
+	int x,y;
+	First(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+	
+	void display()
+	{
+		System.out.println("x:" +x);
+		System.out.println("y:" +y);
+	}
+	
+}
+
+class Second extends First
+{
+	int z;
+	Second(int a, int b, int c)
+	{
+		super(a,b);
+		z = c;
+	}
+	void display()
+	{
+		super.display();
+		System.out.println("z:" + z);
+	}
+	
+}
+public class Overriding {
+	
+	public static void main(String args [])
+	{
+		Second sc = new Second(25,50,100);
+		sc.display();
+	}
+
+}
