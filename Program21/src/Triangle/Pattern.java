c5970d5b2ae11afbfd94eacc08834eda58a8e310
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-10-31 23:57:37 +0530

    Triangular  Pattern-1.

---
+++ Program21/src/Triangle/Pattern.java
@@ -0,0 +1,24 @@
+package Triangle;
+/**
+* @author -- Himanshu Patil
+*/
+public class Pattern {
+	
+	public static void main(String args [])
+	{
+		int x,y,z;
+		for(x=7; x>=1;x--)
+		{
+			for(y=7; y>x; y--)
+			{
+				System.out.print(" ");
+			}
+			for(z=1; z<=x; z++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
