047d6d204f1921a9ca35a801a58dd9572eb68bbc
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-11-01 17:28:18 +0530

    Infinite Loop.

---
+++ Program53/src/InfiniteLoop/Loop.java
@@ -0,0 +1,15 @@
+package InfiniteLoop;
+/**
+* @author -- Himanshu Patil
+*/
+public class Loop {
+	
+	public static void main(String args [])
+	{
+		for(;;)
+		{
+			System.out.println("This is an Infinite Loop");
+		}
+	}
+
+}
