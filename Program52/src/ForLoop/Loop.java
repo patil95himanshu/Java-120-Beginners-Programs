41af6bf50ba01b11d1e360a4d3f94d4c665d0a07
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-11-01 17:27:54 +0530

    For Loop.

---
+++ Program52/src/ForLoop/Loop.java
@@ -0,0 +1,24 @@
+package ForLoop;
+/**
+* @author -- Himanshu Patil
+*/
+public class Loop {
+	
+	public static void main(String args[])
+	{
+		int x,y;
+		
+		for(x=10; x>=1; x--)
+		{
+			System.out.println("Value of x is: " + x);
+		}
+		
+		System.out.println("\n");
+		
+		for(y=1; y<=10; y++)
+		{
+			System.out.println("The value of y is: " + y);
+		}
+	}
+
+}
