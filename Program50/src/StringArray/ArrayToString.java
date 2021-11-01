10d7c32e5c593742ed60a27257a17d5d7df9b976
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-11-01 15:36:04 +0530

    Array to String.

---
+++ Program50/src/StringArray/ArrayToString.java
@@ -0,0 +1,14 @@
+package StringArray;
+import java.util.*;
+/**
+* @author -- Himanshu Patil
+*/
+public class ArrayToString {
+	
+	public static void main(String args [])
+	{
+		int[] x = {9,5,40,33,17,2,0};
+		System.out.println(Arrays.toString(x));
+	}
+
+}
